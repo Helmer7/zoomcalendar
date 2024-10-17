a1576c4b89330085d2c93a7b004109a87424a4e1
Helmer7 <vhelmert@icloud.com>
2024-10-17 18:44:14 -0300

    Adicionando Procfile para o Railway

--- models.py
+++ models.py
@@ -1,3 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 class Reuniao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     topic = db.Column(db.String(255), nullable=False)
